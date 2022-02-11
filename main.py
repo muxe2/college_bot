3d48577914181dac500c5b649d95d3179f4df843
Muxe1 <muxellexum@gmail.com>
2022-02-12 01:00:58 +0300

    Оптимизация

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import requests
 from html2image import Html2Image
 from vk_api import VkApi
 from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
-
+import buttons
 import kb
 
 admins = []
@@ -32,6 +32,7 @@ def send_photo(id, text, photo = None, photo2 = None, keyboard = None):
         photoRequests = requests.post(photoUpload['upload_url'], files={'photo': open(f'{photo2}.png', 'rb')}).json()
         photoMethod = vk.method('photos.saveMessagesPhoto', {'photo': photoRequests['photo'], 'server': photoRequests['server'], 'hash': photoRequests['hash']})[0]
         photo2 = "photo{}_{}".format(photoMethod["owner_id"], photoMethod["id"])
+        
     vk.method("messages.send", {"peer_id": id, "message": text, "attachment": f"{photo},{photo2}", 'keyboard': keyboard, "random_id": 0})
 
 
@@ -42,155 +43,66 @@ def send_photo_render(photo):
     photo = "photo{}_{}".format(photoMethod["owner_id"], photoMethod["id"])
     return photo
 
-
-def main():
+def messages_edit(event, f_toggle, msg, kbrd, attch = None):
+    if msg:
+        if "КУРС" in msg:  
+            attch = "photo-209576287_457239930"       
+    vk_api.messages.edit(
+                        peer_id=event.obj.peer_id,
+                        message=msg,
+                        attachment=attch,
+                        conversation_message_id=event.obj.conversation_message_id,
+                        keyboard=(kbrd if f_toggle else kbrd).get_keyboard(),
+                    )
+    return not f_toggle
+    
+def main():  
     f_toggle: bool = False
     for event in longpoll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW:
                 if event.from_user:
                     msg = event.obj.message["text"].lower()
-                    id = event.obj.message["from_id"]
-              
-                    with open('raspis.json', 'r', encoding='utf-8') as f:
-                        data = json.load(f)
-                                          
+                    id = event.obj.message["from_id"]                                                
                     if "update" in msg:
+                        send_photo(id, "Обновление...")
                         update_raspisanie()
                         send_photo(id, "Расписание обновлено")
                     
                     if "начать" in msg: 
                         vk.method("messages.send", {"peer_id": id, "message": '', "attachment": "photo-209576287_457239928", 'keyboard': kb.keyboard_main.get_keyboard(), "random_id": 0})
                      
-            elif event.type == VkBotEventType.MESSAGE_EVENT:
-                if event.object.payload.get("type") in CALLBACK_TYPES:
-                    vk.messages.sendMessageEventAnswer(
-                        event_id=event.object.event_id,
-                        user_id=event.object.user_id,
-                        peer_id=event.object.peer_id,
-                        event_data=json.dumps(event.object.payload),
-                    )
+            elif event.type == VkBotEventType.MESSAGE_EVENT:      
+                event_type = event.object.payload.get("type")           
+                if event_type in buttons.slov:
+                    messages_edit(event, f_toggle, buttons.slov[event_type]['msg'], buttons.slov[event_type]['kb'], buttons.slov[event_type]['attch'])
                     
-                elif event.object.payload.get("type") == "timetable":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="Расписание",
-                        attachment="photo-209576287_457239929",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_course if f_toggle else kb.keyboard_course).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle 
-                            
-                elif event.object.payload.get("type") == "course_1":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="1 КУРС",
-                        attachment = "photo-209576287_457239930",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_1course if f_toggle else kb.keyboard_1course).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
+                else:    
+                    if event in CALLBACK_TYPES:
+                        vk.messages.sendMessageEventAnswer(
+                            event_id=event.object.event_id,
+                            user_id=event.object.user_id,
+                            peer_id=event.object.peer_id,
+                            event_data=json.dumps(event.object.payload),
+                        )   
+                                                
+                    else:
+                        button = event.object.payload.get("type")
+                        text = button.replace("_button", '')
+                        course = list(text)[0]
+                        with open('raspis.json', 'r', encoding='utf-8') as f:
+                            data = json.load(f)
+                        vk_api.messages.edit(
+                            peer_id=event.obj.peer_id,
+                            message=f"Расписание для {text}",
+                            attachment = data['list'][text],
+                            conversation_message_id=event.obj.conversation_message_id,
+                            keyboard=kb.render(course),
+                        )
+                        f_toggle = not f_toggle
                     
-                elif event.object.payload.get("type") == "course_2":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="2 КУРС",
-                        attachment = "photo-209576287_457239930",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_2course if f_toggle else kb.keyboard_2course).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
-                    
-                elif event.object.payload.get("type") == "course_3":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="3 КУРС",
-                        attachment = "photo-209576287_457239930",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_3course if f_toggle else kb.keyboard_3course).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
-                    
-                elif event.object.payload.get("type") == "course_4":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="4 КУРС",
-                        attachment = "photo-209576287_457239930",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_4course if f_toggle else kb.keyboard_4course).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
-                    
-                elif event.object.payload.get("type") == "МТ_button":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="1 КУРС",
-                        attachment = "photo-209576287_457239930",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_MT if f_toggle else kb.keyboard_MT).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
-                
-                elif event.object.payload.get("type") == "ИП_button":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="1 КУРС",
-                        attachment = "photo-209576287_457239930",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_IP if f_toggle else kb.keyboard_IP).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
-                
-                elif event.object.payload.get("type") == "МР_button":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="3 КУРС",
-                        attachment = "photo-209576287_457239930",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_MR if f_toggle else kb.keyboard_MR).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
-                        
-                elif event.object.payload.get("type") == "К_button":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message="1 КУРС",
-                        attachment = "photo-209576287_457239930",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_K if f_toggle else kb.keyboard_K).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
-                        
-                elif event.object.payload.get("type") == "Отмена_button":
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        attachment= "photo-209576287_457239928",
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=(kb.keyboard_main if f_toggle else kb.keyboard_main).get_keyboard(),
-                    )
-                    f_toggle = not f_toggle
-                            
-                else:
-                    button = event.object.payload.get("type")
-                    text = button.replace("_button", '')
-                    course = list(text)[0]
-                    with open('raspis.json', 'r', encoding='utf-8') as f:
-                        data = json.load(f)
-                    vk_api.messages.edit(
-                        peer_id=event.obj.peer_id,
-                        message=f"Расписание для {text}",
-                        attachment = data['list'][text],
-                        conversation_message_id=event.obj.conversation_message_id,
-                        keyboard=kb.render(course),
-                    )
-                    f_toggle = not f_toggle
-                    
-  
-       
-              
 def update_raspisanie():
     pdfs = ['1k.pdf', '2k.pdf', '3k.pdf', '4k.pdf']
     url = ['http://www.pl130.ru/doc/19/1k.pdf', 'http://www.pl130.ru/doc/19/2k.pdf', 'http://www.pl130.ru/doc/19/3k.pdf', 'http://www.pl130.ru/doc/19/4k.pdf']
-    numbers = []
     
     with open("raspis.json", "rt", encoding="utf-8") as file:
         settings = json.load(file)      
